5516343aeb307760bfe8caf7b6511f768b7fc2cd
우혜영 <@DESKTOP-KS2IJ83>
2021-05-11 01:01:29 +0900

    woooooo

---
+++ src/com/mono/moa/controller/mypage/MyPageReviewList.java
@@ -0,0 +1,24 @@
+package com.mono.moa.controller.mypage;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mono.moa.controller.Controller;
+
+public class MyPageReviewList implements Controller {
+
+	@Override
+	public String exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String view = "/mypage/myPageReviewList";
+		/*
+		 * req.getSession().removeAttribute("SID"); req.setAttribute("isRedirect",
+		 * true);
+		 */
+		
+		return view;
+	}
+
+}
